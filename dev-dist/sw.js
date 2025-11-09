741e32aeba4bd10e0ddc414838025a392d53e388
[dyad] <renato.trabalho17@gmail.com>
2025-11-09 12:58:34 -0300

    [dyad] Integrando o sistema com Supabase para CRUD de funcionários e documentos. - wrote 5 file(s), added @supabase/supabase-js package(s), executed 2 SQL queries + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.l2flpol0rg8"
+    "revision": "0.hmou7cfr1ig"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
