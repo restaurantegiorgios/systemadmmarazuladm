e00d98dd2669899cbf7a7024a88771b9c0c7e320
[dyad] <renato.trabalho17@gmail.com>
2025-11-11 19:56:12 -0300

    [dyad] Atualizando a mensagem de exclusão de funcionário. - wrote 1 file(s) + extra files edited outside of Dyad

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.m6lheu77d48"
+    "revision": "0.n0g9apqf0do"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
